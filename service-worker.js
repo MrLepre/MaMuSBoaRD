0083a58850aafe4c8469a370b7fa456a1af7dcab
MrLepre <multihard.jj@gmail.com>
2026-09-10 14:25:26 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'mamusboard-shell-v18';
-const SHELL = ['./','./index.html','./style.css','./app.js','./manifest.json','./js/core/state.js','./js/core/bootstrap.js','./js/core/supabase.js','./js/realtime.js','./js/auth/auth.js','./js/campaigns/campaigns.js'];
+const CACHE_NAME = 'mamusboard-shell-v19';
+const SHELL = ['./','./index.html','./style.css','./app.js','./manifest.json','./js/core/state.js','./js/core/bootstrap.js','./js/core/supabase.js','./js/realtime.js','./js/auth/auth.js','./js/campaigns/campaigns.js','./js/systems/systems.js'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()));
 });
